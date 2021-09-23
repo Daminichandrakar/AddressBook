72b96b56b51e6a67cb237ac25beb0199ec323c69
Daminichandrakar <damini.2201@gmail.com>
2021-09-24 01:53:14 +0530

    Addred: create a contact in an address book

--- controller/AddressBookController.java
+++ controller/AddressBookController.java
@@ -1,7 +1,10 @@
 package controller;
 
+import model.AddressBookModel;
+
 public class AddressBookController {
 	public static void main(String[] args) {
-		System.out.println("Welcome to addressbook");
+	 AddressBookModel addressBookModel = new AddressBookModel("Damini", "chandrakar", 12345, "D@gmail.com", "chhattishagarh", "Mahasamund", "Raipur", 1234);
+	 System.out.println("Contact Added Successfully");
 	}
 }
